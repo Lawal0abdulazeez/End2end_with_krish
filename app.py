4bcbc5e1390a38987e209ef9cba39b570dd56b1a
Lawal0abdulazeez <lawalabdulazeezfaruq@gmail.com>
2023-02-12 14:07:53 +0100

    On going to deployment

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pickle
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, url_for
 import numpy as np
 
 app=Flask(__name__)
@@ -13,7 +13,6 @@ def home():
     return render_template('home.html')
 
 
-#app.route('/predict_api', method=['POST'])
 @app.route('/predict_api', methods=['POST'])
 def predict_api():
     data=request.json['data']
@@ -23,6 +22,16 @@ def predict_api():
     output=regmodel.predict(new_data)
     print(output)
     return jsonify(output[0])
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html", prediction_text="The house price prediction is {}".format(output))
+
+
 
 
 if __name__=="__main__":
