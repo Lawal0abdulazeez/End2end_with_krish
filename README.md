94aaf0fb6a60ca501f51609e0a8564a1e583d09a
Lawal0abdulazeez <lawalabdulazeezfaruq@gmail.com>
2023-02-19 09:56:56 +0100

    Well explained repo now

--- README.md
+++ README.md
@@ -2,15 +2,25 @@
 This is a repo for learning End2end ML project
 Through a guide from Dr. Krish naik
 A little changes would be made since it has been long since the video was created to the time this repo was made
+The web app would be deployed on render.com
 Praying for the best though, But it might be hard...
 ### Softwre and Tools Requirement
 1. [Github Account](https://github.com)
 2. [vscode](https://code.visualstudio.com/)
-3. [HerokuAccount](https://heroku.com)
-4. [Gitcli](https://)
+3. [RenderAccount](https://render.com)
+4. [Gitcli](https://www.gitcli.com)
 
 Creata a new environment for the project
 
 '''
 conda create -p venv 
 '''
+# To Deploy on Render
+To deploy on render.com is pretty simple, 
+1. You just need to create an account on their website which has been given above
+2. Add a project to your dashboard
+3. Create a procfile in your repo that will be used to start your web app in the cloud
+4. Connect your github repository to the project
+5. Update your requirements.txt file to contain all needed libraries 
+6. And deploy either manually or auto
+
--- app.py
+++ app.py
@@ -1,38 +1,38 @@
-import pickle
-from flask import Flask, request, jsonify, render_template, url_for
-import numpy as np
+import pickle  # To read our pickled model and scaler
+from flask import Flask, request, jsonify, url_for, render_template  # Importing falsk framework and neccesary libraries
+import numpy as np #Importing numpy for data conversion and reshaping
 
-app=Flask(__name__)
+app=Flask(__name__, static_folder='/static')  #initializing flask
 
 ## Load the model
-regmodel= pickle.load(open('regmodel.pkl', 'rb'))
-scalar=pickle.load(open('scaling.pkl','rb'))
+regmodel= pickle.load(open('regmodel.pkl', 'rb'))  # Loading our model from the pickled model file and can now be used in our web app
+scalar=pickle.load(open('scaling.pkl','rb'))      # Loading the scaler from the pickled scaling file and can now be used in our web app
 
-@app.route('/')
+@app.route('/')  # our home page
 def home():
-    return render_template('home.html')
+    return render_template('home.html') # Returns an html file that collect the input of our data and have a button for prediction
 
 
-@app.route('/predict_api', methods=['POST'])
+@app.route('/predict_api', methods=['POST'])   # An local api that can be used with postman 
 def predict_api():
-    data=request.json['data']
+    data=request.json['data']  # request a json file from the host(Postman)
     print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data= scalar.transform(np.array(list(data.values())).reshape(1,-1))
-    output=regmodel.predict(new_data)
+    print(np.array(list(data.values())).reshape(1,-1)) 
+    new_data= scalar.transform(np.array(list(data.values())).reshape(1,-1)) # reshaping the size to fit into the model
+    output=regmodel.predict(new_data)  # predicting and saving our predicted values in a variable
     print(output)
-    return jsonify(output[0])
+    return jsonify(output[0])   # return our predicted value as json to be shown in Postman
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['POST'])  # A web page links to our form and prediction button in our nhtml file
 def predict():
-    data=[float(x) for x in request.form.values()]
-    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    data=[float(x) for x in request.form.values()]  # Turning our form values to float and saving the in a variable
+    final_input=scalar.transform(np.array(data).reshape(1,-1))  # reshaping the size to fit into the model
     print(final_input)
-    output=regmodel.predict(final_input)[0]
-    return render_template("home.html", prediction_text="The house price prediction is {}".format(output))
+    output=regmodel.predict(final_input)[0]     # predicting and saving our predicted values in a variable
+    return render_template("home.html", prediction_text="The house price prediction is {}".format(output))     # return our predicted value and the basic html page
 
 
 
 
-if __name__=="__main__":
-    app.run(debug=True)
+if __name__=="__main__":   # To start the app
+    app.run(debug=True)    # Enabling debugging
